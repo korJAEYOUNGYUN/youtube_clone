6602bb696891bc897e286b119c4de1eb7ae53eec
JAEYOUNGYUN <lastmore9@gmail.com>
2020-01-30 23:52:25 +0900

    Implement basic structure
    Implemented three routers - global, user, video and two controllers - user, video as a simple usage.

---
+++ routers/videoRouter.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import routes from "../routes";
+import {deleteVideo, editVideo, upload, videoDetail, videos} from "../controllers/videoController";
+
+
+const videoRouter = express.Router();
+
+videoRouter.get(routes.home, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.videoDetail, videoDetail);
+
+export default  videoRouter;
