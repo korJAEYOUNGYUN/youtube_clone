54c5cac7c5b3a9f45f15f782a475dbc9d3a87c33
JAEYOUNGYUN <lastmore9@gmail.com>
2020-02-09 23:14:09 +0900

    Implement comments to videos

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,6 @@
 import routes from "../routes";
 import Video from '../models/Video';
+import Comment from '../models/Comment';
 
 
 export const home = async (req, res) => {
@@ -52,7 +53,7 @@ export const videoDetail = async (req, res) => {
     } = req;
 
     try {
-        const video = await Video.findById(id).populate('creator');
+        const video = await Video.findById(id).populate('creator').populate('comments');
         res.render('videoDetail', {pageTitle: video.title, video});
     } catch(error) {
         console.log(error);
@@ -67,7 +68,7 @@ export const getEditVideo = async (req, res) => {
 
     try {
         const video = await Video.findById(id);
-        if(video.creator !== req.user.id) {
+        if(String(video.creator) !== req.user.id) {
             throw Error();
         }
         res.render('editVideo', {pageTitle: `Edit ${video.title}`, video});
@@ -84,7 +85,7 @@ export const postEditVideo = async (req, res) => {
 
     try {
         const video = await Video.findById(id);
-        if(video.creator !== req.user.id) {
+        if(String(video.creator) !== req.user.id) {
             throw Error();
         }
         await Video.findOneAndUpdate({_id: id}, {title, description});
@@ -102,7 +103,7 @@ export const deleteVideo = async (req, res) => {
 
     try {
         const video = await Video.findById(id);
-        if(video.creator !== req.user.id) {
+        if(String(video.creator) !== req.user.id) {
             throw Error();
         }
         await Video.findOneAndRemove({_id: id});
@@ -121,6 +122,27 @@ export const postRegisterView = async (req, res) => {
         video.views += 1;
         video.save();
         res.status(200);
+    } catch(error) {
+        res.status(400);
+    } finally {
+        res.end();
+    }
+};
+
+export const postAddComment = async (req, res) => {
+    const {
+        params: {id},
+        body: {comment},
+        user
+    } = req;
+    try {
+        const video = await Video.findById(id);
+        const newComment = await Comment.create({
+            text: comment,
+            creator: user.id
+        });
+        video.comments.push(newComment.id);
+        video.save();
     } catch(error) {
         res.status(400);
     } finally {
--- routes.js
+++ routes.js
@@ -30,6 +30,7 @@ const FACEBOOK_CALLBACK = '/auth/github/callback';
 // API
 const API = '/api';
 const REGISTER_VIEW = '/:id/view';
+const ADD_COMMENT = '/:id/comment';
 
 const routes = {
     home: HOME,
@@ -80,7 +81,8 @@ const routes = {
     facebook: FACEBOOK,
     facebookCallback: FACEBOOK_CALLBACK,
     api: API,
-    registerView: REGISTER_VIEW
+    registerView: REGISTER_VIEW,
+    addComment: ADD_COMMENT
 };
 
 export default routes;
